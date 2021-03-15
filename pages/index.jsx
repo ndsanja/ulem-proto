233635428701071b8dac1fed5da656fb0c45a2e2
ndsanja <ndsanja@gmail.com>
2021-03-15 08:50:44 +0700

    relatif page builder

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,52 +4,6 @@ import Sidebar from "../components/Sidebar";
 import { useState } from "react";
 
 export default function index() {
-  const menu = [
-    {
-      id: 1,
-      category: "panitia",
-      list: ["event organizer", "wedding organizer"],
-    },
-    {
-      id: 2,
-      category: "administrasi",
-      list: ["proposal", "anggaran", "perizinan"],
-    },
-    {
-      id: 3,
-      category: "kalender",
-      list: ["list", "jadwal", "kalender"],
-    },
-    {
-      id: 4,
-      category: "undangan",
-      list: ["daftar tamu", "buku tamu", "design undangan"],
-    },
-    {
-      id: 5,
-      category: "dokumentasi",
-      list: ["foto", "video", "galeri"],
-    },
-    {
-      id: 6,
-      category: "akomodasi",
-      list: [
-        "tempat",
-        "sound sistem",
-        "dekorasi",
-        "transportasi",
-        "konsumsi",
-        "souvenir",
-        "Wardrobe & Make Up",
-      ],
-    },
-    {
-      id: 7,
-      category: "acara",
-      list: ["roundown", "MC", "hiburan", "perangkat acara", "multimedia"],
-    },
-  ];
-
   const [isOpen, setIsOpen] = useState(false);
   const [page, setPage] = useState("");
   return (
